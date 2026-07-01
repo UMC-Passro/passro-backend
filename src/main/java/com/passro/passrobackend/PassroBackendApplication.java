235c7cf356773ae5f6c47667ed17f4fed25d1b90
Park Wonho <olivyou2@gmail.com>
2026-07-01 19:49:29 +0900

    chore: first commit

---
+++ src/main/java/com/passro/passrobackend/PassroBackendApplication.java
@@ -0,0 +1,13 @@
+package com.passro.passrobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PassroBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PassroBackendApplication.class, args);
+    }
+
+}
