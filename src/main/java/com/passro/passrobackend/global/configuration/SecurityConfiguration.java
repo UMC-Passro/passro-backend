d8a55f848fb99aa0b97266a72190c5fd36d9f823
Park Wonho <olivyou2@gmail.com>
2026-07-04 19:56:37 +0900

    chore: swagger 적용

--- src/main/java/com/passro/passrobackend/global/configuration/SecurityConfiguration.java
+++ src/main/java/com/passro/passrobackend/global/configuration/SecurityConfiguration.java
@@ -20,8 +20,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth ->
-                        auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                        .anyRequest().authenticated())
                 .build();
     }
 
