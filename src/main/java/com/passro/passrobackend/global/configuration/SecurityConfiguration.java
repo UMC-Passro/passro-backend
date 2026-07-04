f661ed9090aec9655e73ac69c945d75842f6777a
Park Wonho <olivyou2@gmail.com>
2026-07-04 17:34:30 +0900

    chore: SecurityConfiguration 세팅

---
+++ src/main/java/com/passro/passrobackend/global/configuration/SecurityConfiguration.java
@@ -0,0 +1,28 @@
+package com.passro.passrobackend.global.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/static/**", "/h2-console/**");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth ->
+                        auth.anyRequest().authenticated())
+                .build();
+    }
+
+}
