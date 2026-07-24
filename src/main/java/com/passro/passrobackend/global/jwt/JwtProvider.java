ce6d86e7b380e2940c76c0a2d0536a76a6f9a38e
JYH <wjddydghks0801@gmail.com>
2026-07-24 12:57:53 +0900

    JwtProvider 구현

---
+++ src/main/java/com/passro/passrobackend/global/jwt/JwtProvider.java
@@ -0,0 +1,72 @@
+package com.passro.passrobackend.global.jwt;
+
+
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.nio.charset.StandardCharsets;
+import java.util.Date;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+
+    private final JWTProperties jwtProperties;
+
+    private SecretKey getSigninKey(){
+        return Keys.hmacShaKeyFor(jwtProperties.getSecret().getBytes(StandardCharsets.UTF_8));
+    }
+
+    private String createAccessToken(Long accountId, String role){
+        return createToken(accountId, role, jwtProperties.getAccessTokenExpiration());
+    }
+
+    private String createRefreshToken(Long accountId, String role){
+        return createToken(accountId, null, jwtProperties.getRefreshTokenExpiration());
+    }
+
+    private String createToken(Long accountId, String role, long expiration){
+        Date now = new Date();
+        Date expiry = new Date(now.getTime()  + expiration);
+
+        JwtBuilder builder = Jwts.builder()
+                .subject(String.valueOf(accountId))
+                .issuedAt(now)
+                .expiration(expiry)
+                .signWith(getSigninKey());
+
+        if(role != null)
+            builder.claim("role", role);
+
+        return builder.compact();
+    }
+
+    public Long getAccountId(String token) {
+        return Long.parseLong(parseClaims(token).getSubject());
+    }
+
+    /** 토큰에서 role 추출 */
+    public String getRole(String token) {
+        return parseClaims(token).get("role", String.class);
+    }
+
+    private Claims parseClaims(String token){
+        return Jwts.parser()
+                .verifyWith(getSigninKey())
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            parseClaims(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
