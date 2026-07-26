ee729d8f2f257fa6d7501eba5656113c7a7edf32
JYH <wjddydghks0801@gmail.com>
2026-07-26 17:12:43 +0900

    permitAll에서 logout 제외

--- src/main/java/com/passro/passrobackend/account/controller/AccountController.java
+++ src/main/java/com/passro/passrobackend/account/controller/AccountController.java
@@ -11,6 +11,7 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +20,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Validated
+@RequestMapping("/subway")
 public class AccountController {
 
     private final AccountService accountService;
