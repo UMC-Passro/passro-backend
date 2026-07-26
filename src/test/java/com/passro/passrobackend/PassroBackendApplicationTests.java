7f1042cab790e585883208ef30100b7be93d583d
Park Wonho <olivyou2@gmail.com>
2026-07-26 18:18:16 +0900

    deploy: workflow 에 test 루틴 추가

--- src/test/java/com/passro/passrobackend/PassroBackendApplicationTests.java
+++ src/test/java/com/passro/passrobackend/PassroBackendApplicationTests.java
@@ -1,10 +1,22 @@
 package com.passro.passrobackend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.mysql.MySQLContainer;
 
 @SpringBootTest
+@Testcontainers
 class PassroBackendApplicationTests {
+
+    @Container
+    @ServiceConnection
+    static final MySQLContainer mysql = new MySQLContainer("mysql:8.4")
+            .withDatabaseName("passro_test")
+            .withUsername("passro")
+            .withPassword("passro");
 
     @Test
     void contextLoads() {
