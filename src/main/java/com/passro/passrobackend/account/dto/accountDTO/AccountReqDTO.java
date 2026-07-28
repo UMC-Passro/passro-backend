695290fc21b38d17b330f9e827954c78646034b6
JYH <wjddydghks0801@gmail.com>
2026-07-28 11:25:44 +0900

    닉네임 Edit 클래스 정의

---
+++ src/main/java/com/passro/passrobackend/account/dto/accountDTO/AccountReqDTO.java
@@ -0,0 +1,19 @@
+package com.passro.passrobackend.account.dto.accountDTO;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+public class AccountReqDTO {
+
+    @Getter
+    public static class Edit {
+        @NotBlank(message = "변경할 닉네임을 입력하세요.")
+        @Pattern(
+                regexp = "^[가-힣a-zA-Z0-9]+$",
+                message = "닉네임은 완성된 한글, 영문, 숫자만 입력 가능합니다."
+        )
+        private String nickname;
+    }
+}
