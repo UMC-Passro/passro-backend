59d1aa6ec753a802f04f166fdffc57ad794a9300
JYH <wjddydghks0801@gmail.com>
2026-07-30 10:39:25 +0900

    search 로직 변경에 따른 기존 subwayApiService 삭제

--- src/main/java/com/passro/passrobackend/account/dto/SubwayApiResDTO.java
+++ src/main/java/com/passro/passrobackend/account/dto/SubwayApiResDTO.java
@@ -3,8 +3,6 @@ package com.passro.passrobackend.account.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
