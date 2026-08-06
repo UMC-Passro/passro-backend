c0447822ad27dfd5230b99b276197b5006eeb81e
JYH <wjddydghks0801@gmail.com>
2026-08-07 02:20:27 +0900

    AccountController 수정

--- src/main/java/com/passro/passrobackend/account/controller/AccountController.java
+++ src/main/java/com/passro/passrobackend/account/controller/AccountController.java
@@ -9,6 +9,7 @@ import com.passro.passrobackend.global.code.BaseSuccessCode;
 import com.passro.passrobackend.global.configuration.security.CustomUserDetails;
 import com.passro.passrobackend.global.response.APIResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.CacheControl;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
+@Tag(name = "계정", description = "프로필 조회 및 수정")
 public class AccountController {
 
     private final AccountService accountService;
@@ -43,6 +45,7 @@ public class AccountController {
     }
 
     @PatchMapping("/mypage/edit/myInfo")
+    @ApiResponse(responseCode = "200", description = "마이페이지 수정 성공", useReturnTypeSchema = true)
     public ResponseEntity<APIResponse<Void>> editNickname(@Valid @RequestBody AccountReqDTO.EditMyInfo dto, @AuthenticationPrincipal CustomUserDetails userDetails){
         BaseSuccessCode code = AccountSuccessCode.OK;
         accountService.editMyInfo(dto, userDetails.getAccountId());
@@ -52,6 +55,7 @@ public class AccountController {
     }
 
     @PatchMapping("/mypage/edit/password/mail")
+    @ApiResponse(responseCode = "200", description = "메일 요청 성공", useReturnTypeSchema = true)
     public APIResponse<Void> sendPasswordEditMail(@AuthenticationPrincipal CustomUserDetails userDetails){
         BaseSuccessCode code = AccountSuccessCode.OK;
         mailSenderService.sendMailMessageEditPassword(userDetails.getAccountId());
@@ -59,6 +63,7 @@ public class AccountController {
     }
 
     @PatchMapping("/mypage/edit/password")
+    @ApiResponse(responseCode = "200", description = "비밀번호 변경 성공", useReturnTypeSchema = true)
     public APIResponse<Void> editPassword(@Valid @RequestBody AccountReqDTO.EditPassword dto, @AuthenticationPrincipal CustomUserDetails userDetails){
         BaseSuccessCode code = AccountSuccessCode.OK;
         accountService.codeCodeConfirmAndEditPassword(dto, userDetails.getAccountId());
