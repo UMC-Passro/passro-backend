9d4dd8b837e47ed356dbc60e1ca943a250db3fb7
wjddydghks <wjddydghks0801@gmail.com>
2026-08-07 10:26:00 +0900

    EditPassword dto 현재비밀번호, 수정할 비밀번호 필드 조합으로 수정

--- src/main/java/com/passro/passrobackend/account/dto/accountDTO/AccountReqDTO.java
+++ src/main/java/com/passro/passrobackend/account/dto/accountDTO/AccountReqDTO.java
@@ -35,13 +35,14 @@ public class AccountReqDTO {
 
     @Getter
     public static class EditPassword {
+        @NotBlank(message = "현재 비밀번호를 입력하세요.")
+        @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하여야 합니다.")
+        @PasswordComplexity
+        private String nowPassword;
+
         @NotBlank(message = "변경할 비밀번호를 입력하세요.")
         @Size(min = 6, max = 20, message = "비밀번호는 6자 이상 20자 이하여야 합니다.")
         @PasswordComplexity
-        private String password;
-
-        @NotBlank(message="인증 코드를 입력하세요")
-        @Pattern(regexp = "^[0-9]{6}$", message = "인증 코드는 숫자 6자리여야 합니다.")
-        private String code;
+        private String editPassword;
     }
 }
